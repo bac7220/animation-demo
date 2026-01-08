2a9281f291787e354c3a20b1d0518b75f5e33cbb
bac7220 <bacbac7220@gmail.com>
2026-01-09 07:39:50 +0900

    各ファイルインデント修正

--- bg-slide/script.js
+++ bg-slide/script.js
@@ -1,10 +1,9 @@
-console.log('test');  
+console.log("test");
 const observe = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('is-active');
+      entry.target.classList.add("is-active");
     }
-  })
-})
-document.querySelectorAll('.js-slide').forEach((el => observe.observe(el)));
-
+  });
+});
+document.querySelectorAll(".js-slide").forEach((el) => observe.observe(el));
